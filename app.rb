170e2cd201dea8609e244eb2576a8f66e7bcb552
Dom-R <Dom-R@users.noreply.github.com>
2018-02-27 05:54:31 +0000

    removed unusued route

--- app.rb
+++ app.rb
@@ -2,10 +2,6 @@ require 'sinatra'
 require 'pry-byebug' unless production?
 require_relative 'lib/identificator'
 
-get '/' do
-  'Hello world!'
-end
-
 post '/webhooks' do
   Identificator.identify(request)
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,22 +1,6 @@
 require 'spec_helper'
 
 describe "App" do
-  describe 'received get at' do
-    describe '/' do
-      it 'responds ok' do
-        get '/'
-
-        expect(last_response).to be_ok
-      end
-
-      it 'says hello world' do
-        get '/'
-
-        expect(last_response.body).to eq 'Hello world!'
-      end
-    end
-  end
-
   describe 'received post at' do
     describe '/webhooks' do
       it 'responds ok' do
