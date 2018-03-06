981483adfe3bd165dfe8a8cca0fb4181bab7e635
Dom-R <Dom-R@users.noreply.github.com>
2018-03-06 05:16:56 +0000

    fixed tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -5,16 +5,37 @@ require 'spec_helper'
 RSpec.describe 'App' do
   describe 'received post at' do
     describe '/' do
-      it 'responds ok' do
-        post '/'
+      let(:parameters) { {} }
+      let(:enviroments) { {} }
 
-        expect(last_response).not_to be_ok
+      subject do
+        post '/', params = parameters, rack_env = enviroments
       end
 
       it 'calls Identificator.identify' do
         expect(Identificator).to receive(:identify)
 
         post '/'
+      end
+
+      context 'pull request request' do
+        let(:enviroments) { { 'HTTP_X_GITHUB_EVENT' => 'pull_request' } }
+
+        it 'responds ok' do
+          subject
+
+          expect(last_response).to be_ok
+        end
+      end
+
+      context 'other requests' do
+        let(:enviroments) { { 'SOME_RANDOM_HEADER' => 'something unecessary' } }
+
+        it 'responds 412' do
+          subject
+
+          expect(last_response).not_to be_ok
+        end
       end
     end
   end
