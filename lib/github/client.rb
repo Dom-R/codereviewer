9b6ca667bb76f0860caf023897de32dd24f54ad0
Dom-R <Dom-R@users.noreply.github.com>
2018-03-11 22:55:30 +0000

    created github client

---
+++ lib/github/client.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'base64'
+require 'net/http'
+require 'json'
+
+module Github
+  class Client
+    def post(url, body)
+      authorization = Base64.strict_encode64("#{ENV['GITHUB_USERNAME']}:#{ENV['TOKEN']}")
+      header = { 'Authorization' => "Basic #{authorization}",
+                 'Content-Type' => 'application/json' }
+
+      uri = URI(url)
+      Net::HTTP.post(uri, body.to_json, header)
+    end
+  end
+end
---
+++ spec/lib/github/client_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Github::Client do
+  describe '#post' do
+    let!(:url) { 'https://api.github.com/url' }
+    let!(:body) do
+      {
+        body: 'this is a body'
+      }
+    end
+    let!(:header) do
+      {
+        'Authorization' => 'Basic Og==',
+        'Content-Type' => 'application/json'
+      }
+    end
+
+    it 'sends a post request' do
+      expect(Net::HTTP).to receive(:post).with(
+        URI('https://api.github.com/url'),
+        body.to_json,
+        header
+      )
+
+      subject.post(url, body)
+    end
+  end
+end
